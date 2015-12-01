00b4c1fe77a2f77fceec979c570526a3702b0c12
Rafael Fiume <rafael.fiume@yahoo.it>
2015-12-01 20:13:36 -0200

    Acceptance test passes (WiP)

---
+++ README.md
@@ -0,0 +1,7 @@
+# Prosciutto-Mob [![Build Status](https://travis-ci.org/rafaelfiume/Prosciutto-Mob.svg?branch=master)](https://travis-ci.org/rafaelfiume/Prosciutto-Mob)
+
+## Running the App
+
+Run the tests from the command-line typing:
+
+    $./gradlew cAT     or    ./gradlew connectedAndroidTest
--- app/src/androidTest/java/com/rafaelfiume/prosciutto/adviser/test/AdviserEndToEndTest.java
+++ app/src/androidTest/java/com/rafaelfiume/prosciutto/adviser/test/AdviserEndToEndTest.java
@@ -1,65 +1,37 @@
 package com.rafaelfiume.prosciutto.adviser.test;
 
-import android.support.test.espresso.NoMatchingViewException;
-import android.support.test.espresso.ViewAssertion;
-import android.support.test.espresso.core.deps.guava.base.Optional;
-import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.espresso.util.HumanReadables;
 import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.AssertionFailedError;
-import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.LinearLayout;
 
 import com.rafaelfiume.prosciutto.adviser.AdviserActivity;
 import com.rafaelfiume.prosciutto.adviser.Product;
 import com.rafaelfiume.prosciutto.adviser.R;
 
 import org.apache.commons.io.IOUtils;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkArgument;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.test.espresso.util.TreeIterables.breadthFirstViewTraversal;
-import static android.support.test.internal.util.Checks.checkNotNull;
 import static com.rafaelfiume.prosciutto.adviser.R.id.expert_option;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.hasToString;
-import static org.hamcrest.Matchers.hasValue;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-@RunWith(AndroidJUnit4.class)
 public class AdviserEndToEndTest {
 
     @Rule
@@ -108,68 +80,6 @@ public class AdviserEndToEndTest {
 
     }
 
-    private static Matcher<View> withAdaptedData(final Matcher<Object> dataMatcher) {
-        return new TypeSafeMatcher<View>() {
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("with class name: ");
-                dataMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                if (!(view instanceof AdapterView)) {
-                    return false;
-                }
-                @SuppressWarnings("rawtypes")
-                Adapter adapter = ((AdapterView) view).getAdapter();
-                for (int i = 0; i < adapter.getCount(); i++) {
-                    if (dataMatcher.matches(adapter.getItem(i))) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-        };
-    }
-
-    public static Matcher<Object> withItemContent(String expectedText) {
-        checkNotNull(expectedText);
-        return withItemContent(equalTo(expectedText));
-    }
-
-    public static Matcher<Object> withItemContent(final Matcher<String> itemTextMatcher) {
-        checkNotNull(itemTextMatcher);
-        return new BoundedMatcher<Object, Product>(Product.class) {
-            @Override
-            public boolean matchesSafely(Product p) {
-                return itemTextMatcher.matches(p.name());
-            }
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("with item content: ");
-                itemTextMatcher.describeTo(description);
-            }
-        };
-    }
-
-
-    BoundedMatcher<Object, Product> hasProduct(final String expectedName) {
-        return new BoundedMatcher<Object, Product>(Product.class) {
-
-            @Override
-            protected boolean matchesSafely(Product item) {
-                return item.name().equals(expectedName);
-            }
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("expected product: " + expectedName);
-            }
-        };
-    }
     //
     // TODO RF 31/10/15 Move these tests to another place
     // Checking if request/response test setup is all right
